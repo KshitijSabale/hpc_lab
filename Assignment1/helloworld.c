c08971828c0bf8c50d87467ab78bc4bee31e29fb
kshitisabale14 <kshitisabale14@gmail.com>
2022-11-28 12:00:59 +0530

    all

--- Assignment1/helloworld.c
+++ Assignment1/helloworld.c
@@ -5,8 +5,12 @@
 void main()
 {   
     printf("Hello World\n");
-    #pragma omp parallel
-    {
+    // omp_set_num_threads(5);
+    #pragma omp parallel for ordered
+    for(int i=0;i<5;i++)
+    {   
+        #pragma omp ordered
         printf("Hello from thread number %d \n",omp_get_thread_num());
     }
 }
+
